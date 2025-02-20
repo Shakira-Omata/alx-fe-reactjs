6a9348793b899f24ba4b5b5fdce3c0c175770396
Shakira Omata <osajipurity3@gmail.com>
2025-02-20 13:25:39 +0300

    thee

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,4 +1,7 @@
 import WelcomeMessage from './components/WelcomeMessage'; 
+import Footer from './components/Footer';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
 
 
 
@@ -13,6 +16,9 @@ function App() {
   return (
     <div>
       <WelcomeMessage />
+      <Header />
+      <MainContent />
+      <Footer />
     </div>
   );
 }
