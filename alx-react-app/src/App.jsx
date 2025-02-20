010f76eb7c3ad88232a4bb631875676ecd4c38d9
Shakira Omata <osajipurity3@gmail.com>
2025-02-20 13:37:21 +0300

    four

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,9 +2,7 @@ import WelcomeMessage from './components/WelcomeMessage';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import MainContent from './components/MainContent';
-
-
-
+import UserProfile from './components/UserProfile'; 
 
 
 
@@ -19,6 +17,7 @@ function App() {
       <Header />
       <MainContent />
       <Footer />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
     </div>
   );
 }
