eb062916b48ebdc1ea6e62cd9033c21c234eef0a
Shakira Omata <osajipurity3@gmail.com>
2025-02-20 13:12:18 +0300

    one

---
+++ alx-react-app/src/App.jsx
@@ -0,0 +1,20 @@
+import WelcomeMessage from './components/WelcomeMessage'; 
+
+
+
+
+
+
+
+
+
+
+function App() {
+  return (
+    <div>
+      <WelcomeMessage />
+    </div>
+  );
+}
+
+export default App;
