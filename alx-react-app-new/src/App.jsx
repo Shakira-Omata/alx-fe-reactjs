b5c7191aa3508119ae6f45690d0c593816f0bce7
Shakira Omata <osajipurity3@gmail.com>
2025-02-22 10:28:42 +0300

    two

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -3,6 +3,9 @@ import Footer from './components/Footer';
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import UserProfile from './components/UserProfile'; 
+import Counter from './components/Counter';
+
+
 
 
 
@@ -17,6 +20,7 @@ function App() {
       <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       <MainContent />
       <Footer />
+      <Counter />
       
     </div>
   );
