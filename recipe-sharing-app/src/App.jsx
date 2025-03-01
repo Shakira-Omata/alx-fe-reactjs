0dcd971593750deb4049ff6bd54d33d71b557747
Shakira Omata <osajipurity3@gmail.com>
2025-03-01 08:43:11 +0300

    one

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,16 @@
+import AddRecipeForm from "./components/AddRecipeForm.jsx"; 
+import RecipeList from "./components/RecipeList.jsx";
+
+
+function App() {
+  return (
+    <div>
+      <h1>Recipe Sharing App</h1>
+      <AddRecipeForm />
+      <RecipeList />
+    </div>
+  );
+}
+
+export default App;
+
