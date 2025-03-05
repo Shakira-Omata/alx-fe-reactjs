839ef1dd81a2b488c4dc64684b4c5be7d6394376
Shakira Omata <osajipurity3@gmail.com>
2025-03-05 12:02:40 +0300

    one

---
+++ form-handling-react/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import FormikForm from './components/FormikForm';
+import RegistrationForm from './components/RegistrationForm';
+
+
+const App = () => {
+  return (
+    <div>
+      <h1>Controlled Form</h1>
+      <registrationForm />
+      <h1>Formik Form</h1>
+      <FormikForm />
+    </div>
+  );
+};
+
+
+export default App;
---
+++ form-handling-react/src/components/RegistrationForm.jsx
@@ -0,0 +1,68 @@
+import React, { useState } from 'react';
+
+
+const RegistrationForm = () => {
+    const [formData, setFormData] = useState({
+        username: '',
+        email: '',
+        password: ''
+    });
+
+    const [errors, setErrors] = useState({});
+
+    const handleChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        });
+    };
+
+    const validate = () => {
+        const newErrors = {};
+        if (!formData.username) newErrors.username = 'Username is required';
+        if (!formData.email) newErrors.email = 'Email is required';
+        if (!formData.password) newErrors.password = 'Password is required';
+        return newErrors;
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const validationErrors = validate();
+        if (Object.keys(validationErrors).length > 0) {
+            setErrors(validationErrors);
+        }else {
+            console.log('Form Submitted:', formData);
+            setFormData({ username: '', email: '', password: '' });
+            setErrors({});
+        }
+    };
+    
+    return (
+        <form onSubmit={handleSubmit} className='registration-form'>
+            <div>
+                <label>Username:</label>
+                <input type='text' name='username' value={formData.username} onChange={handleChange} />
+                {errors.username && <p className='error'>{errors.username}</p>}
+            </div>
+            <div>
+                <label>Email:</label>
+                <input type='email' name='email' value={formData.email} onChange={handleChange} />
+                {errors.email && <p className='error'>{errors.email}</p>}
+            </div>
+
+            <div>
+                <label>Password:</label>
+                <input type='password' name='password' value={formData.password} onChange={handleChange} />
+                {errors.password && <p className='error'>{errors.password}</p>}
+            </div>
+
+            <button type='submit'>Register</button>
+        </form>
+    );
+};
+
+export default RegistrationForm;
+
+           
+
+       
