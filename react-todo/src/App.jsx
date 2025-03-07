da3c9276c768f9c9e62416925634fa2a08427313
Shakira Omata <osajipurity3@gmail.com>
2025-03-07 15:18:12 +0300

    shak

---
+++ react-todo/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import TodoList from "./components/TodoList"; // ✅ Ensure this is correct
+
+function App() {
+    return (
+        <div>
+            <h1>My Todo App</h1>
+            <TodoList />
+        </div>
+    );
+}
+
+export default App;
