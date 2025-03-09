d69939051c3c0f151fc72213b6bd85756eb9a206
Shakira Omata <osajipurity3@gmail.com>
2025-03-09 12:46:38 +0300

    shak

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import NotFound from "./pages/NotFound";
+import BlogPost from "./components/BlogPost";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/profile/*" element={<Profile />} />
         <Route path="*" element={<NotFound />} />
+        <Route path="/blog/:id" element={<BlogPost />} />
       </Routes>
     </Router>
   );
