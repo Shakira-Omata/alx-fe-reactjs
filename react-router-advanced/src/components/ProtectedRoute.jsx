645c8783a7a7488a05f08d5e27b2ca3b878019c1
Shakira Omata <osajipurity3@gmail.com>
2025-03-09 12:50:05 +0300

    shak

--- react-router-advanced/src/components/ProtectedRoute.jsx
+++ react-router-advanced/src/components/ProtectedRoute.jsx
@@ -1,4 +1,5 @@
 import { Navigate } from "react-router-dom";
+import useAuth from "../hooks/useAuth";
 
 const ProtectedRoute = ({ children }) => {
   const isAuthenticated = false; // Change this based on actual authentication logic
