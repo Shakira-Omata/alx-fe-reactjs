ce6cd5885688ecdc64741fcbb4437856068bee37
Shakira Omata <osajipurity3@gmail.com>
2025-03-30 18:54:30 +0300

    shak

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,12 +1,21 @@
 import axios from "axios";
 
-const BASE_URL = "https://api.github.com/users/";
+const BASE_URL = "https://api.github.com/search/users?q=";
 
-export const fetchUserData = async (username) => {
-  try {
-    const response = await axios.get(`${BASE_URL}${username}`);
-    return response.data;
-  } catch (error) {
-    throw new Error("User not found");
-  }
-};
+export const fetchUsers = async (username, location, minRepos, page = 1) => {
+    try {
+      let query = "";
+  
+      if (username) query += `${username} in:login `;
+      if (location) query += `location:${location} `;
+      if (minRepos) query += `repos:>${minRepos} `;
+  
+      const response = await axios.get(`${BASE_URL}${query.trim()}&page=${page}&per_page=10`);
+      return response.data.items || [];
+    } catch (error) {
+      throw new Error("User search failed");
+    }
+  };
+  
+
+
