67011f3ee8fe87a4ac7cebd5c17e43deeaeeef2a
Shakira Omata <osajipurity3@gmail.com>
2025-03-30 19:37:46 +0300

    shak

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const BASE_URL = "https://api.github.com/users/";
 
+
 export const fetchUserData = async (username) => {
   try {
     const response = await axios.get(`${BASE_URL}${username}`);
@@ -14,17 +15,16 @@ export const fetchUserData = async (username) => {
   }
 };
 
-export const fetchUsers = async (username, location, minRepos, page= 1) => {
-    try{
-        let query = "";
-        if (username) query += `${username} in:login `;
-        if (location) query += `location:${location} `;
-        if (minRepos) query += `repos:>${minRepos} `;
-    
+const GITHUB_API_URL = "https://api.github.com/search/users?q=";
 
-        const response = await axios.get(`${BASE_URL}${query.trim()}&page=${page}&per_page=10`);
-        return response.data.items || [];
-      } catch (error) {
-        throw new Error("User search failed");
-      }
-    };
+export const fetchUsers = async (username, location, minRepos, page) => {
+  try {
+    const query = `${username}${location ? "+location:" + location : ""}${minRepos ? "+repos:>" + minRepos : ""}&page=${page}`;
+    const response = await axios.get(`${GITHUB_API_URL}${query}`);
+    return response.data.items;
+  } catch (error) {
+    console.error("Error fetching users: ", error);
+    throw error;
+  }
+};
+
