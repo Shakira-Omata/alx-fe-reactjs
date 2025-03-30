53a818590eb65ddf2d9fd1b70528227e15fb78ed
Shakira Omata <osajipurity3@gmail.com>
2025-03-30 19:30:11 +0300

    shak

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -13,3 +13,18 @@ export const fetchUserData = async (username) => {
     throw new Error("Something went wrong");
   }
 };
+
+export const fetchUsers = async (username, location, minRepos, page= 1) => {
+    try{
+        let query = "";
+        if (username) query += `${username} in:login `;
+        if (location) query += `location:${location} `;
+        if (minRepos) query += `repos:>${minRepos} `;
+    
+
+        const response = await axios.get(`${BASE_URL}${query.trim()}&page=${page}&per_page=10`);
+        return response.data.items || [];
+      } catch (error) {
+        throw new Error("User search failed");
+      }
+    };
